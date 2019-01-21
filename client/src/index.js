e27847b5b587869d8449720ba28d96a5d3f8b078
student <student@Cjs-MacBook-Pro.local>
2019-01-21 00:27:46 -0500

    Removed https redirect package in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,19 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Route } from 'react-router-dom';
-import HttpsRedirect from 'react-https-redirect';
 import * as serviceWorker from './serviceWorker';
 import Container from './components/screens/Container';
 
 const router = (
-    <HttpsRedirect >
-        <BrowserRouter >
-            <div>
-                <Route exact path="/" component={App} />
-                <Route path="/search" component={Container} />
-            </div>
-        </BrowserRouter>
-    </HttpsRedirect>
+    <BrowserRouter >
+        <div>
+            <Route exact path="/" component={App} />
+            <Route path="/search" component={Container} />
+        </div>
+    </BrowserRouter>
 )
 
 ReactDOM.render(router, document.getElementById('root'));
